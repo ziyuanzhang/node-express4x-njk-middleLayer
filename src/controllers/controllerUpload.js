5d7bea6ab3f4c4e79afd361c7d5c6f55a44bc377
ziyuzhan <ziyuzhan@cisco.com>
2018-10-03 20:53:40 +0800

    优化 form 提交

--- src/controllers/controllerUpload.js
+++ src/controllers/controllerUpload.js
@@ -1,25 +1,12 @@
-
-//https://www.npmjs.com/package/formidable  上传文件用插件
-var formidable = require('formidable');
-let customizeUtil = require('../middleware/customizeUtil')
-const targetFile = './upload';
+let UploadData = require('../middleware/formUpload');
 
 exports.upload = function (req, res) {
-    var form = new formidable.IncomingForm();
-    form.uploadDir = customizeUtil.createFolder(targetFile);
-    form.keepExtensions = true;
-    form.encoding = 'utf-8';
-    form.maxFieldsSize = 2 * 1024 * 1024;
-    form.maxFileSize = 200 * 1024 * 1024
-    form.parse(req,function(err,fields,files){
-        if(err){
-           throw new Error(err);
-        }
+    UploadData.saveUploadData(req,function(fields,files){
         console.log('fields:',fields); 
         console.log('files:',files);        
 
         res.writeHead(200,{"Content-Type":"text/html;charset=UTF8"});
         res.end('表单提交成功！');
-    });
+    })
   
 }
---
+++ src/middleware/formUpload.js
@@ -0,0 +1,20 @@
+
+//https://www.npmjs.com/package/formidable  上传文件用插件
+let formidable = require('formidable');
+let customizeUtil = require('../middleware/customizeUtil')
+const targetFile = './upload';
+
+exports.saveUploadData= function(req,callback){
+    var form = new formidable.IncomingForm();
+    form.uploadDir = customizeUtil.createFolder(targetFile);
+    form.keepExtensions = true;
+    form.encoding = 'utf-8';
+    form.maxFieldsSize = 2 * 1024 * 1024;
+    form.maxFileSize = 200 * 1024 * 1024
+    form.parse(req,function(err,fields,files){
+        if(err){
+           throw new Error(err);
+        }       
+        callback(fields,files);
+    });
+}
